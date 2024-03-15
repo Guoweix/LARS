4301198532741853f92b788c3b8a95ef7c548dd5
GuoWeix <2298701336@qq.com>
2024-03-15 10:42:05 +0800

    addi.w测试代码

--- test/addi.w.asm
+++ test/addi.w.asm
@@ -8,7 +8,7 @@ addi.w $t0,$t0,100
 addi.w $t0,$t0,-100
 
 addi.w $a0,$a0,2047
-addi.w $a1,$a1,2048
+#addi.w $a1,$a1,2048
 addi.w $a2,$a2,-2048
-addi.w $t0,$t0,1000000000
+#addi.w $t0,$t0,1000000000
 
